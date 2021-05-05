7e7772647d859f90a4460256b8ad71ff96c8468e
546pvp <>
2021-05-05 16:27:47 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const answer = prompt("age")
+if (answer > 12) {
+  alert("sorry this site is not available for your age!")
+} else if (answer < 10) {
+  alert(" sorry this site is not available for your age!")
+} else if (answer == 11, 10) {
+  let message = `Hi! Thanks for visiting our site!`
+  alert(message)
+}
+
